eac9b5e611d05864fa011d043bf798492b12eb79
letícia_fran <124750949+leticiafrancielle@users.noreply.github.com>
2023-05-24 13:34:33 -0300

    Update Main.java

--- Listas/Lista_8/exerc_01/src/Main.java
+++ Listas/Lista_8/exerc_01/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) throws NomeRepetididoException {
+    public static void main(String[] args) {
         Grupo grupo = new Grupo();
         try {
             grupo.adicionarHeroi(new Heroi(1, "Lyn", Arma.ESPADA));
