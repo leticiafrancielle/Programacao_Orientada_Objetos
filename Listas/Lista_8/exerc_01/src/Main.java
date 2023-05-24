08f7a47bbffae6b287d5d002cf27f72fb127c303
leticiafrancielle <leticiafrancielle2005@gmail.com>
2023-05-23 23:33:51 -0300

    exercícios programação_orientada_objetos

---
+++ Listas/Lista_8/exerc_01/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) throws NomeRepetididoException {
+        Grupo grupo = new Grupo();
+        try {
+            grupo.adicionarHeroi(new Heroi(1, "Lyn", Arma.ESPADA));
+            grupo.adicionarHeroi(new Heroi(2, "Marcus", Arma.LANCA));
+            grupo.adicionarHeroi(new Heroi(3, "Dorcas", Arma.MACHADO));
+            grupo.adicionarHeroi(new Heroi(4, "Marcus", Arma.LANCA));
+        } catch (NomeRepetididoException exception) {
+            System.out.println("Os heróis não podem ter nome repitido!");
+        }
+    }
+}
